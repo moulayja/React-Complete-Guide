c0b2535f0752d150c27734b84904e92fb9a9109f
Jamal <moulayjam@gmail.com>
2020-03-26 15:36:36 -0400

    Passing State Through Props

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons : [
+      {name: 'Jamalino', age: 36},
+      {name: 'Catilino', age: 34},
+      {name: 'Medino', age: 37}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello, I am React App</h1>
         <p>This is working App</p>
-        <Person name='Jamal' age = '21'/>
-        <Person name = 'Cait' age = '20'> My Hobbies are: Reading and cooking</Person>
-        <Person name = 'Med' age = '22'/>
+        <Person name={this.state.persons[0].name} age = {this.state.persons[0].age}/>
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}> My Hobbies are: Reading and cooking</Person>
+        <Person name = {this.state.persons[2].name} age =  {this.state.persons[2].age}/>
       </div>
-
     );
   }
 }
