d063705e9e1232dae05a3d216a9f9aabd54e6d2c
Jamal <moulayjam@gmail.com>
2020-03-26 14:38:19 -0400

    Rendring JSX

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello React</h1>
+        <h1>Hello, I am React App</h1>
       </div>
     );
   }
