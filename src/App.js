bea0a8d7796f4ea56d688ffba57c75e8d5fa2c9f
Jamal <moulayjam@gmail.com>
2020-03-26 14:52:36 -0400

    Creating a Functional Component | Person

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <Fragment className="App">
         <h1>Hello, I am React App</h1>
         <p>This is working App</p>
+        <Person />
       </Fragment>
 
     );
